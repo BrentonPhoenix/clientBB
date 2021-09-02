bd820cbcdc9e841ab9d6a5620800b920230ee817
Richard Carter <rbc.coding@gmail.com>
2021-09-02 15:55:03 -0500

    Header and Navbar, App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css'
 import './App.css';
+
+import Footer from './site/Footer';
+import Header from './site/Header';
+import {
+  BrowserRouter as Router
+} from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="main">
-      
+    <div className="App">
+      <Router>
+      <Header />
+      </Router>
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
