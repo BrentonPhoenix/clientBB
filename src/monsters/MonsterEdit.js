becaa4d999cb50f9931fe09f5412b895effe2db9
ChristianSpielman <cspielman10752@gmail.com>
2021-09-03 09:39:58 -0400

    create/edit update

--- src/monsters/MonsterEdit.js
+++ src/monsters/MonsterEdit.js
@@ -13,9 +13,9 @@ const MonsterEdit = (props) => {
 
     const monsterUpdate = (event, monster) => {
         event.preventDefault();
-        fetch(`http://localhost:4000/log/update/${props.monsterToUpdate.id}`, {
+        fetch(`http://localhost:4000/my-monsters/update/${props.monsterToUpdate.id}`, {
             method: 'PUT',
-            body: JSON.stringify({monster: {creature: editcreature, image: editimage, campaign: editcampaign, hitpoints: edithitpoints, armorclass: editarmorclass, speed: editspeed, rating: editrating, description: editdescription}}),
+            body: JSON.stringify({log: {creature: editcreature, image: editimage, campaign: editcampaign, hitpoints: edithitpoints, armorclass: editarmorclass, speed: editspeed, rating: editrating, description: editdescription}}),
             headers: new Headers({
                 'Content-Type': 'application/json',
                 'Authorization': `Bearer ${props.token}`
