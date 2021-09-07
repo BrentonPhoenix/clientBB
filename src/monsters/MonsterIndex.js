35929d0cdd2c4147b8c3607c6edf616e7ba7ed32
BrentonPhoenix <brentonphoenix@gmail.com>
2021-09-07 13:24:05 -0400

    small changes to app.css and monsterindex

--- src/monsters/MonsterIndex.js
+++ src/monsters/MonsterIndex.js
@@ -54,8 +54,8 @@ const MonsterIndex = (props) => {
             
             {updateActive ? <MonsterEdit monsterToUpdate={monsterToUpdate} updateOff={updateOff} token={props.token} fetchMonsters=
             {fetchMonsters}/> : <></>}
-            {/* <MonsterCreate fetchMonsters={fetchMonsters} token={props.token}/>
-            <MonsterTable monsters={monsters} editUpdateMonster={editUpdateMonster} updateOn={updateOn} fetchMonsters={fetchMonsters} token={props.token}/> */}
+            <MonsterCreate fetchMonsters={fetchMonsters} token={props.token}/>
+            <MonsterTable monsters={monsters} editUpdateMonster={editUpdateMonster} updateOn={updateOn} fetchMonsters={fetchMonsters} token={props.token}/>
         </div>
 
     )
