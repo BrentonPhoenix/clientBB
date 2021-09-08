cd6146de5b30df47c0d92e5278dbd3ba2b9cd095
BrentonPhoenix <brentonphoenix@gmail.com>
2021-09-08 15:44:02 -0400

    work this time

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -4,7 +4,7 @@ switch(window.location.hostname){
     case 'localhost' || '127.0.0.1':
         APIURL = 'http://localhost:4000';
         break;
-    case 'pb-rl-cs-rc-dm-app.herokuapp.com':
+    case 'team-2-monster-tracker.herokuapp.com':
         APIURL = 'https://pb-rl-cs-rc-dm-app.herokuapp.com'
     
 }
