a31e3042853822aba218d1c90074460545db48a5
BrentonPhoenix <brentonphoenix@gmail.com>
2021-09-08 15:38:48 -0400

    changed server url

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -4,10 +4,11 @@ switch(window.location.hostname){
     case 'localhost' || '127.0.0.1':
         APIURL = 'http://localhost:4000';
         break;
-    case 'git.heroku.com/pb-rl-cs-rc-dm-app.git':
-        APIURL = 'https://git.heroku.com/pb-rl-cs-rc-dm-app.git'
+    case 'pb-rl-cs-rc-dm-app.herokuapp.com':
+        APIURL = 'https://pb-rl-cs-rc-dm-app.herokuapp.com'
+    
 }
-
+// did not push case two changes yet
 
 export default APIURL
 
